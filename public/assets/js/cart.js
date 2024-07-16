04e9d4f80be7be3c78eb6496b0e3b7f7e15b7a46
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-07-15 21:26:21 -0300

    Configurando o axios

---
+++ public/assets/js/cart.js
@@ -0,0 +1,8 @@
+import http from './services/http';
+async function getProducts() {
+  try {
+    const {data} = await http.get('?inc=get-products');
+  } catch (error) {
+    console.log(error);
+  }
+}
