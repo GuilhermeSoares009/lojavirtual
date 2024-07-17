96464cb0ee8f7d1c99e31618f36f3ed2855a3048
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-07-16 22:52:00 -0300

    corrigindo função do carrinho de compras

--- app/functions/cart.php
+++ app/functions/cart.php
@@ -10,10 +10,10 @@ function addToCart(int $productId)
     $product = first('products', 'price');
 
     if (!isset($_SESSION['cart'][$productId])){
-        $_SESSION['cart'][$productId] = ['qty' => 1, 'price' => $product->price];
+        $_SESSION['cart'][$productId] = ['qty' => 1, 'subtotal' => $product->price];
     }else{
         $qty = $_SESSION['cart'][$productId]['qty'];
-        $_SESSION['cart'][$productId] = ['qty' => $qty+=1, 'price' => $product->price * $qty];
+        $_SESSION['cart'][$productId] = ['qty' => $qty+=1, 'subtotal' => $product->price * $qty];
     }
 }
 
