3256e9d86dda24def171d240070d7b952fec16b4
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-08-03 09:34:03 -0300

    Adicionando Auth

---
+++ app/functions/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+function auth()
+{
+    if (isAuth()) {
+        return (object)$_SESSION['user'];
+    }
+}
+
+function fullName() {
+    return auth()->firstName. ' ' .auth()->lastName;
+}
+
+function isAuth(){
+    return isset($_SESSION['user']);
+}
+
+
+function logout(){
+    if(isAuth())
+    {
+        unset($_SESSION['user']);
+        return redirect('/');
+    }
+}
